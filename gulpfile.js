837e046bdb507ca2fa8f0ca7a6c13bfefdb5c415
苏令浩 <1377852243@qq.com>
2017-03-19 15:41:53 +0800

    approve gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -15,22 +15,62 @@ let count = 1;
 let server = http.createServer((req, res) => {
 	console.log(count++);
 	let pathName = url.parse(req.url).pathname || 'index.html';
-	// pathName = pathName === '/'? 'src' + pathName + 'index.html' : 'src' + pathName
-	// let path = pathname.split('/');
 	console.log(pathName);
-	if(pathName.slice(-3) === 'css'){
-		res.writeHead(200,{'Content-Type':'text/css'});
-	}else{
-		res.writeHead(200,{'Content-Type':'text/html'});
-	}
-	fs.readFile( '/毕业设计' + pathName,'utf8',(err,data) => {
-		if(err){
-			// throw err;
-		}
-		res.end(data)
 
-	})
+	switch(pathName){
+		case ''||'/':
+			parseFiles('/index.html', res);
+			break;
+		default: 
+			parseFiles(pathName);
+			break;
+	}
+
 })
+
+let parseFiles = (url,res) => {
+	let fileName = url.slice(1);
+	let type = getType(fileName.lastIndexOf('.') + 1);
+
+	fs.readFile('./src/' + fileName, (err, data) => {
+		if(err){
+			res.writeHead(404, { 'Content-Type':'text/plain; charset="UTF-8"' });
+			res.write(err.message);
+			res.end();
+		} else {
+			res.writeHead(200, { 'Content-Type': type });
+			res.write(data);
+			res.end();
+		}
+	})
+}
+
+let getType = (endTag) => {
+	var type = null;
+	switch (endTag) {
+		case 'html':
+		case 'htm':
+			type = 'text/html; charset=UTF-8';
+			break;
+		case 'js':
+			type = 'application/javascript; charset="UTF-8"';
+			break;
+		case 'css':
+			type = 'text/css; charset="UTF-8"';
+			break;
+		case 'txt':
+			type = 'text/plain; charset="UTF-8"';
+			break;
+		case 'manifest':
+			type = 'text/cache-manifest; charset="UTF-8"';
+			break;
+		default:
+			type = 'application/octet-stream';
+			break;
+	}
+	return type;
+}
+
 
 gulp.task('default', function(){
 	server.listen(config.proxy,config.server,()=>{
